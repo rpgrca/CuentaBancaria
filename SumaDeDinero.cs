cdcbafc252bedd9a2d72fa50d5f06423e3417da4
Roberto <rpgrca@gmail.com>
2020-11-13 01:05:34 -0300

    add test, sumadedinero can't be negative

--- SumaDeDinero.cs
+++ SumaDeDinero.cs
@@ -1,15 +1,20 @@
+using System;
+
 namespace oop
 {
     public class SumaDeDinero
     {
+        public const string NO_SE_PUEDE_CREAR_UNA_SUMA_DE_DINERO_NEGATIVA = "No se puede crear una suma de dinero negativa.";
+
         public decimal Total { get; }
 
-        private SumaDeDinero(decimal sumaDeDinero) =>
-            Total = sumaDeDinero;
-
-        public static SumaDeDinero De(decimal sumaDeDinero)
+        private SumaDeDinero(decimal sumaDeDinero)
         {
-            return new SumaDeDinero(sumaDeDinero);
+            if (sumaDeDinero < 0) throw new ArgumentException(NO_SE_PUEDE_CREAR_UNA_SUMA_DE_DINERO_NEGATIVA);
+            Total = sumaDeDinero;
         }
+
+        public static SumaDeDinero De(decimal sumaDeDinero) =>
+            new SumaDeDinero(sumaDeDinero);
     }
 }
--- SumaDeDineroDebe.cs
+++ SumaDeDineroDebe.cs
@@ -1,6 +1,4 @@
-using System.Runtime;
-using System.Runtime.InteropServices;
-using System.Reflection;
+using System;
 using Xunit;
 
 namespace oop
@@ -8,11 +6,18 @@ namespace oop
     public class SumaDeDineroDebe
     {
         [Fact]
-        public void Test1()
+        public void RetornarSumaDeDinero_CuandoSeCreaConValorValido()
         {
             var sut = SumaDeDinero.De(1000);
             Assert.NotNull(sut);
             Assert.Equal(1000, sut.Total);
+        }
+
+        [Fact]
+        public void LanzarExcepcion_CuandoSeCreaConValorInvalido()
+        {
+            var exception = Assert.Throws<ArgumentException>(() => SumaDeDinero.De(-1));
+            Assert.Equal(SumaDeDinero.NO_SE_PUEDE_CREAR_UNA_SUMA_DE_DINERO_NEGATIVA, exception.Message);
         }
     }
 }
