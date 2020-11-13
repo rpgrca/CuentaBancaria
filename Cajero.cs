710f6bcf2397c2b4a8bf81aa1d8a1ae326aea2ce
Roberto <rpgrca@gmail.com>
2020-11-13 00:17:11 -0300

    convert cajero to expression body

--- Cajero.cs
+++ Cajero.cs
@@ -2,19 +2,13 @@ namespace oop
 {
     public class Cajero
     {
-        public decimal ConsultarSaldoDe(Cuenta cuenta)
-        {
-            return cuenta.Saldo;
-        }
+        public decimal ConsultarSaldoDe(Cuenta cuenta) =>
+            cuenta.Saldo;
 
-        public void DepositarEn(Cuenta cuenta, decimal unaSumaDeDinero)
-        {
+        public void DepositarEn(Cuenta cuenta, decimal unaSumaDeDinero) =>
             cuenta.Depositar(unaSumaDeDinero);
-        }
 
-        public void ExtraderDe(Cuenta cuenta, decimal unaSumaDeDinero)
-        {
+        public void ExtraderDe(Cuenta cuenta, decimal unaSumaDeDinero) =>
             cuenta.Extraer(unaSumaDeDinero);
-        }
     }
 }
