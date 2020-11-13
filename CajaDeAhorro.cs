e0705d8d7c4275801823c2bced8fa0a33e918974
Roberto <rpgrca@gmail.com>
2020-11-12 23:43:29 -0300

    add test, can't deposit negative values

--- CajaDeAhorro.cs
+++ CajaDeAhorro.cs
@@ -5,11 +5,17 @@ namespace oop
     public class CajaDeAhorro
     {
         public const string NO_SE_PUEDE_EXTRAER_MAS_QUE_EL_SALDO = "No se puede extraer más dinero que el saldo existente.";
+        public const string NO_SE_PUEDE_DEPOSITAR_UNA_DEUDA = "No se puede depositar una cantidad negativa de dinero.";
 
         public decimal Saldo { get; private set; } = 0;
 
         public void Depositar(decimal unaCantidadDeDinero)
         {
+            if (unaCantidadDeDinero < 0)
+            {
+                throw new ArgumentException(NO_SE_PUEDE_DEPOSITAR_UNA_DEUDA);
+            }
+
             Saldo += unaCantidadDeDinero;
         }
 
