21e29a9ad73bc93813629109f934cf9f782d95c2
Roberto <rpgrca@gmail.com>
2020-11-12 23:54:10 -0300

    add test, can't extract negative values

--- CuentaCorriente.cs
+++ CuentaCorriente.cs
@@ -17,6 +17,7 @@ namespace oop
 
         public void Extraer(decimal unaCantidadDeDinero)
         {
+            if (unaCantidadDeDinero < 0) throw new ArgumentException(CajaDeAhorro.NO_SE_PUEDE_EXTRAER_UNA_DEUDA);
             if (Saldo - unaCantidadDeDinero <= -1000) throw new ArgumentException(NO_SE_PUEDE_EXTRAER_MAS_ALLA_DEL_LIMITE);
 
             Saldo -= unaCantidadDeDinero;
