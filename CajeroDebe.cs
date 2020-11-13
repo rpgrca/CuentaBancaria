81845c690f3de2c1d971e4adecfe35a1a297bf45
Roberto <rpgrca@gmail.com>
2020-11-13 00:16:15 -0300

    add more tests for cajero

--- CajeroDebe.cs
+++ CajeroDebe.cs
@@ -35,5 +35,34 @@ namespace oop
 
             Assert.Equal(1000, sut.ConsultarSaldoDe(cuentaCorriente));
         }
+
+        [Fact]
+        public void ExtraerDineroDeCuentaCorriente()
+        {
+            var cuentaCorriente = new CuentaCorriente();
+            var sut = new Cajero();
+            sut.DepositarEn(cuentaCorriente, 1000);
+            sut.ExtraderDe(cuentaCorriente, 250);
+
+            Assert.Equal(750, sut.ConsultarSaldoDe(cuentaCorriente));
+        }
+
+        [Fact]
+        public void LanzarExcepcion_CuandoSeExtraeDeMasDeCajaDeAhorro()
+        {
+            var cajaDeAhorro = new CajaDeAhorro();
+            var sut = new Cajero();
+            var exception = Assert.Throws<ArgumentException>(() => sut.ExtraderDe(cajaDeAhorro, 1));
+            Assert.Equal(CajaDeAhorro.NO_SE_PUEDE_EXTRAER_MAS_QUE_EL_SALDO, exception.Message);
+        }
+
+        [Fact]
+        public void DejarSaldoNegativo_CuandoSeExtraeDeMasDeCuentaCorriente()
+        {
+            var cuentaCorriente = new CuentaCorriente();
+            var sut = new Cajero();
+            sut.ExtraderDe(cuentaCorriente, 1);
+            Assert.Equal(-1, sut.ConsultarSaldoDe(cuentaCorriente));
+        }
     }
 }
